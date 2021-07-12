d9c93aafda3b0f1039af2fe04dea01782d2ca6cd
alejandrozeb <alejandrojzeballos@gmail.com>
2021-07-12 18:31:37 -0400

    {add} Instalacion de docker-compose

---
+++ apuntes.js
@@ -0,0 +1,19 @@
+/*
+Docker compose es la parte que se encarga de administrar varios docker files
+
+Permite tener diferentes docker files y administrarlo con
+docker compose
+
+los procesos se define en el archivo docker compose
+ya no construimos container por container
+es mas rapido ademas incluye redes, volumenes.
+
+ya no se hara de manera manual la creacion de estos ahora vamos a orquestra sus funciones
+
+https://docs.docker.com/compose/install/
+
+documentacion de docker compose
+no tuve probelmas de instalacion siguiendo los pasos de la doc en ubuntu
+
+
+*/
