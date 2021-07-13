b3080c983015485907b7bfab612f2a4d88e7c3f5
alejandrozeb <alejandrojzeballos@gmail.com>
2021-07-13 18:50:32 -0400

    {add} docker-compose levantando proyecto en php

--- apuntes.js
+++ apuntes.js
@@ -26,5 +26,52 @@ RUN docker-php-ext-install mysqli \
 la sintaxis de la instlacion es propio de la instalacion
 
 si usamos otros frameworks podemos tener esta base de php
+ya no usaremos docker bulid tmbien podemos usar la extencion de vs
+en el dockerfile tenemos un container la onfiguracion necesaria para que se levante
+todo lo que va en enviroment
+con compose se puede manejar diferentes container a basede uno
+comopse orquesta.
+
+version3
+elegimos las que queramos
+services
+son containers en base a una imagen
+app
+es el nombre que tiene el container
+restart
+se levanta el container y lo intenta 3 veces
+build
+indicamos donde esta el docker file del servicio
+volume
+reflejamos las modificaciones
+espeja los cambios
+ports 
+inidicamos primero el puerto de la maquina y segundo el pùerto del container
+envirement
+son las variables de ambiente del container
+
+------comandos--------------
+
+docker-compose build
+inidicamos que contruya las imagenes
+lo hace en automatico
+y crea el contenedor descargando la imagen y los siguientes pasos
+
+
+docker-compose up
+para levantar el proyecto.
+
+docker-compose up -d 
+para enviarlo de fondo
+
+al entrar al puerto se puede ver las configuraciones 
+
+en el proyecto inicialmente se esta imprimiento php info.
+ya podemos trabajar en la app
+docker-compose down 
+para bajar el container
+te ayuda a evitar muchas configuraciones con docker
+
+
 
 */
